2a060b396128ab58be95399a734557dd53953c39
alny <nygaard14@gmail.com>
2019-01-20 09:42:41 +0100

    Config

--- components/Auth/auth0-variables.ts
+++ components/Auth/auth0-variables.ts
@@ -1,5 +1,5 @@
 export const AUTH_CONFIG = {
   domain: "drixmix.eu.auth0.com",
   clientId: "GeMzhONUu4BFiheds3CwqjGID6vnm8Nh",
-  callbackUrl: "https://vactv-deploy-test.herokuapp.com/callback"
+  callbackUrl: "https://vactv-web.herokuapp.com/callback"
 };
